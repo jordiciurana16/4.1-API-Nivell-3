14c68498056f0c182eef07b4a6968ec0a10a1e79
jordiciurana16 <jciuranas@gmail.com>
2025-02-28 17:44:21 +0100

    store & update joke rating

--- index.js
+++ index.js
@@ -1,46 +1,98 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", () => {
-  const joke = document.querySelector("h5");
-  const button = document.querySelector("button");
-  const score = document.querySelector("h6");
-
-  const stars = document.querySelectorAll("span");
-
-  if (!joke || !button) {
-    console.error("There was an error loading the page");
-    return;
-  }
-  async function fetchJoke() {
-    try {
-      const response = await fetch("https://icanhazdadjoke.com/", {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error HTTP: ${response.status}`);
-      }
-
-      const data = await response.json();
-      joke.textContent = data.joke;
-    } catch (error) {
-      joke.style.color = "red";
-      joke.textContent = error.message;
-    }
-  }
-
-  fetchJoke();
-
-  button.addEventListener("click", () => {
-    fetchJoke();
-  });
-
-  score.addEventListener("mouseover", (event) => {
-    const hoveredStarIndex = Array.from(stars).indexOf(event.target);
-    stars.forEach((star, index) => {
-      star.innerHTML = index <= hoveredStarIndex ? "&#9733;" : "&#9734;";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  });
+};
+const jokeReports = [];
+document.addEventListener("DOMContentLoaded", () => {
+    const joke = document.querySelector("h5");
+    const button = document.querySelector("button");
+    const score = document.querySelector("h6");
+    const stars = document.querySelectorAll("span");
+    if (!joke || !button || !score) {
+        console.error("There was an error loading the page");
+        return;
+    }
+    let selectedStarIndex;
+    function fetchJoke() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const response = yield fetch("https://icanhazdadjoke.com/", {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                    },
+                });
+                if (!response.ok) {
+                    throw new Error(`Error HTTP: ${response.status}`);
+                }
+                const data = yield response.json();
+                if (joke) {
+                    joke.textContent = data.joke;
+                }
+                else {
+                    console.error("Joke element is null");
+                }
+                const jokeData = {
+                    id: data.id,
+                    text: data.joke,
+                    rating: undefined,
+                };
+                jokeReports.push(jokeData);
+                console.log(jokeReports);
+            }
+            catch (error) {
+                if (error instanceof Error && joke) {
+                    joke.style.color = "red";
+                    joke.textContent = error.message;
+                }
+                else {
+                    console.error("Joke element is null or error is not an instance of Error");
+                }
+            }
+        });
+    }
+    function updateJokeRating(rating) {
+        if (jokeReports.length > 0) {
+            jokeReports[jokeReports.length - 1].rating = rating;
+        }
+    }
+    function resetScore() {
+        stars.forEach((star) => {
+            star.innerHTML = "&#9734;";
+        });
+        selectedStarIndex = -1;
+    }
+    fetchJoke();
+    resetScore();
+    button.addEventListener("click", () => {
+        fetchJoke();
+        resetScore();
+    });
+    score.addEventListener("mouseover", (event) => {
+        const hoveredStarIndex = Array.from(stars).indexOf(event.target);
+        stars.forEach((star, index) => {
+            star.innerHTML = index <= hoveredStarIndex ? "&#9733;" : "&#9734;";
+        });
+    });
+    score.addEventListener("mouseout", () => {
+        stars.forEach((star, index) => {
+            star.innerHTML = index <= selectedStarIndex ? "&#9733;" : "&#9734;";
+        });
+    });
+    score.addEventListener("click", (event) => {
+        const clickedStarIndex = Array.from(stars).indexOf(event.target);
+        if (clickedStarIndex !== -1) {
+            selectedStarIndex = clickedStarIndex;
+            updateJokeRating((selectedStarIndex + 1));
+        }
+        stars.forEach((star, index) => {
+            star.innerHTML = index <= selectedStarIndex ? "&#9733;" : "&#9734;";
+        });
+    });
 });
--- index.ts
+++ index.ts
@@ -1,24 +1,118 @@
+"use strict";
+
+type Joke = {
+  readonly id: string;
+  text: string;
+  rating?: Score;
+};
+
+type Score = undefined | 1 | 2 | 3;
+
+const jokeReports: Joke[] = [];
+
 document.addEventListener("DOMContentLoaded", () => {
   const joke: HTMLElement | null = document.querySelector("h5");
-  if (!joke) {
-    console.error("The h5 element does not exist");
+  const button = document.querySelector("button");
+  const score = document.querySelector("h6");
+
+  const stars = document.querySelectorAll("span");
+
+  if (!joke || !button || !score) {
+    console.error("There was an error loading the page");
     return;
   }
-  fetch("https://icanhazdadjoke.com/", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
+
+  let selectedStarIndex: number | undefined;
+
+  async function fetchJoke() {
+    try {
+      const response = await fetch("https://icanhazdadjoke.com/", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
       if (!response.ok) {
         throw new Error(`Error HTTP: ${response.status}`);
       }
-      return response.json();
-    })
-    .then((data) => (joke.textContent = data.joke))
-    .catch((error) => {
-      joke.style.color = "red";
-      joke.textContent = error;
+
+      const data = await response.json();
+      if (joke) {
+        joke.textContent = data.joke;
+      } else {
+        console.error("Joke element is null");
+      }
+
+      const jokeData: Joke = {
+        id: data.id,
+        text: data.joke,
+        rating: undefined,
+      };
+
+      jokeReports.push(jokeData);
+      console.log(jokeReports);
+    } catch (error) {
+      if (error instanceof Error && joke) {
+        joke.style.color = "red";
+        joke.textContent = error.message;
+      } else {
+        console.error(
+          "Joke element is null or error is not an instance of Error"
+        );
+      }
+    }
+  }
+
+  function updateJokeRating(rating: Score) {
+    if (jokeReports.length > 0) {
+      jokeReports[jokeReports.length - 1].rating = rating;
+    }
+  }
+
+  function resetScore() {
+    stars.forEach((star) => {
+      star.innerHTML = "&#9734;";
     });
+    selectedStarIndex = -1;
+  }
+
+  fetchJoke();
+  resetScore();
+
+  button.addEventListener("click", () => {
+    fetchJoke();
+    resetScore();
+  });
+
+  score.addEventListener("mouseover", (event) => {
+    const hoveredStarIndex = Array.from(stars).indexOf(
+      event.target as HTMLSpanElement
+    );
+
+    stars.forEach((star, index) => {
+      star.innerHTML = index <= hoveredStarIndex ? "&#9733;" : "&#9734;";
+    });
+  });
+
+  score.addEventListener("mouseout", () => {
+    stars.forEach((star, index) => {
+      star.innerHTML = index <= selectedStarIndex ? "&#9733;" : "&#9734;";
+    });
+  });
+
+  score.addEventListener("click", (event) => {
+    const clickedStarIndex = Array.from(stars).indexOf(
+      event.target as HTMLSpanElement
+    );
+
+    if (clickedStarIndex !== -1) {
+      selectedStarIndex = clickedStarIndex;
+      updateJokeRating((selectedStarIndex + 1) as Score);
+    }
+
+    stars.forEach((star, index) => {
+      star.innerHTML = index <= selectedStarIndex ? "&#9733;" : "&#9734;";
+    });
+  });
 });
