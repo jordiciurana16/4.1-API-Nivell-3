7f40c477dfbd62dba13a2c7da6938fa5c708fbe8
jordiciurana16 <jciuranas@gmail.com>
2025-02-28 14:21:15 +0100

    joke api implementation & button

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 "use strict";
-const message = "Hello, World!";
-const body = document.querySelector("body");
-const paragraph = document.createElement("p");
-paragraph.textContent = message;
-document.body.appendChild(paragraph);
+document.addEventListener("DOMContentLoaded", () => {
+  const joke = document.querySelector("h5");
+  const button = document.querySelector("button");
+
+  if (!joke || !button) {
+    console.error("There was an error loading the page");
+    return;
+  }
+  async function fetchJoke() {
+    try {
+      const response = await fetch("https://icanhazdadjoke.com/", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error HTTP: ${response.status}`);
+      }
+
+      const data = await response.json();
+      joke.textContent = data.joke;
+    } catch (error) {
+      joke.style.color = "red";
+      joke.textContent = error.message;
+    }
+  }
+
+  fetchJoke();
+
+  button.addEventListener("click", () => {
+    fetchJoke();
+  });
+});
--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
-const message = "Hello, World!";
-
-const body = document.querySelector("body");
-
-const paragraph = document.createElement("p");
-
-paragraph.textContent = message;
-
-document.body.appendChild(paragraph);
+document.addEventListener("DOMContentLoaded", () => {
+  const joke: HTMLElement | null = document.querySelector("h5");
+  if (!joke) {
+    console.error("The h5 element does not exist");
+    return;
+  }
+  fetch("https://icanhazdadjoke.com/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error HTTP: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => (joke.textContent = data.joke))
+    .catch((error) => {
+      joke.style.color = "red";
+      joke.textContent = error;
+    });
+});
